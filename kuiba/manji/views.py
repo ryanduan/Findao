954cb6388e15f1c659c1c11772f4f5d3e274ae37
ryanduan <root@ThinkPad.(none)>
2013-07-29 00:36:24 +0800

    login&logout

--- kuiba/manji/views.py
+++ kuiba/manji/views.py
@@ -2,27 +2,53 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from models import FindaoUserInfo, FindaoTag, FindaoShare
-from forms import UserForm
-from db_control import addUser
+from forms import RegistUserForm, LoginUserForm
+from db_control import addUser, findUser
+from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
+import hashlib
+
+def logout(req):
+    del req.session['user']
+    return HttpResponseRedirect('/index/')
+
+def login(req):
+    if req.method == 'POST':
+	uf = LoginUserForm(req.POST)
+	if uf.is_valid():
+	    username = uf.cleaned_data['username']
+	    pw = uf.cleaned_data['password']
+	    password = hashlib.md5(pw).hexdigest()
+	    user = findUser(username, password)
+	    if user:
+		req.session['user'] = user
+		return render_to_response('index.html',{'user':user})
+	    else:
+		uf = LoginUserForm()
+		errorinfo = '用户或密码不正确！'
+	        return render_to_response('login.html',{'uf':uf, 'errorinfo':errorinfo})
+    else:
+
+        uf = LoginUserForm()
+    return render_to_response('login.html',{'uf':uf})
 
 def regist(req):
     if req.method == 'POST':
-	uf = UserForm(req.POST)
+	uf = RegistUserForm(req.POST)
 	if uf.is_valid():
 	    username = uf.cleaned_data['username']
 	    p1 = uf.cleaned_data['password1']
 	    p2 = uf.cleaned_data['password2']
 	    if p1 == p2:
-		password = p1
+		password = hashlib.md5(p1).hexdigest()
 		addUser(username, password)
 	        return HttpResponseRedirect('/index/')
 	    else:
 		errorinfo = '两次密码不匹配'
-                uf = UserForm()
+                uf = RegistUserForm()
             return render_to_response('regist.html',{'uf':uf, 'errorinfo':errorinfo})
     else :
-        uf = UserForm()
+        uf = RegistUserForm()
     return render_to_response('regist.html',{'uf':uf})
 	
 
