e9ed4d9ccaeaeb0242ce1bc3bf8bcf439a0afd31
ryanduan <root@ThinkPad.(none)>
2013-07-28 17:30:17 +0800

    simple db design

---
+++ kuiba/manji/models.py
@@ -0,0 +1,32 @@
+#coding:utf8
+from django.db import models
+from django.contrib.auth.models import User
+
+class FindaoUserInfo(models.Model):
+    user = models.OneToOneField(User)
+    gender = models.CharField(max_length=1, choices=(('m','男'),('f','女')), default='m', blank=True)
+    birthday = models.DateField(default='1990-01-01')
+    ctime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user.username
+
+class FindaoTag(models.Model):
+    tagname = models.CharField(max_length=40)
+    ctime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.tagname
+
+class FindaoShare(models.Model):
+    tags = models.ManyToManyField(FindaoTag)
+    whose = models.ForeignKey(User)
+    title = models.CharField(max_length=60)
+    codes = models.TextField()
+    ctime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    
+
